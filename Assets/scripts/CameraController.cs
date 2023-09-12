beca5791fcbd1ac2cca962e7f02798abc1a993c8
bhaddock1 <bhaddock25@sandomenico.org>
2023-09-12 14:16:36 -0700

    2.7 mid

--- Assets/scripts/CameraController.cs
+++ Assets/scripts/CameraController.cs
@@ -1,21 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+/********************************************
+ * Component of the main camera that keeps it 
+ * a constant distance from the vehicle
+ * 
+ * Bryce Haddock
+ * September 12,2023 Version 1.0
+ *******************************************/
 
 public class CameraController : MonoBehaviour
-{
-    [SerializeField] private Transform vehicleTransform;
-    private Vector3 offset;
 
-    // Start is called before the first frame update
+{   [Tooltip("drag vehicle onto Vehicle Transform")]
+    [SerializeField] private Transform vehicleTransform;// to keep track of vehicle's position
+    private Vector3 offset;// distance camera is from vehicle
+
+    // Initializes the field at start
     void Start()
     {
-        offset = new Vector3(0, 5, -7);
+        offset = new Vector3(0, 5, -7);// Camera's offset position
     }
 
-    // Update is called once per frame
+    // Updates camera position per frame just after Update
     void LateUpdate()
     {
-        transform.position = vehicleTransform.position + offset;
+        transform.position = vehicleTransform.position + offset;// to keep camera following vehicle position
     }
 }
--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -1,21 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
+/********************************************
+ * Component of vehicle that moves it at a constant speed forward
+ * 
+ * Bryce Haddock
+ * Spetember 12, 2023 Version 1.0
+ *******************************************/
 
 public class PlayerController : MonoBehaviour
 {
-    private float speed = 20;
+    private float speed;// holds foward movement of vehicle
+    private float turnSpeed;
+    private float verticalInput;
+    private float horizantalInput;
 
-    // Start is called before the first frame update
+    private Rigidbody rb;
+
+
+    // initializes speed before first frame update
     void Start()
     {
         
+        speed = 20.0f;// Constant speed of vehicle
+        turnSpeed = 30.0f;
+        rb = GetComponent<Rigidbody>();
+   
     }
 
-    // Update is called once per frame
+    // Updates vehicle postion per frame on Update
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
-        
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);// position of vehicle ever frame update 
+
     }
+
+    private void OnMove(InputValue input)
+    {
+        verticalInput = input.Get<Vector2>()
+    }
+
+
 }
+
