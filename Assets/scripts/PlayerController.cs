1d90ece250bca9aed2e0e025b9140475739f3d69
bhaddock1 <bhaddock25@sandomenico.org>
2023-09-13 14:33:49 -0700

    2.7 done

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -12,34 +12,36 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    private float speed;// holds foward movement of vehicle
-    private float turnSpeed;
-    private float verticalInput;
-    private float horizantalInput;
+    private float speed;                // holds foward movement of vehicle
+    private float turnSpeed;            // holds the turn speed of the vehicle
+    private float verticalInput;        // gets a value [-1,1] from user key press up/down W/S
+    private float horizantalInput;      // gets a value [-1,1] from user key press left/right A/D
 
-    private Rigidbody rb;
+    private Rigidbody rb;               // points to vehicle rigid body component
 
 
-    // initializes speed before first frame update
+    // initializes speed and turn speed before first frame update
     void Start()
     {
         
-        speed = 20.0f;// Constant speed of vehicle
-        turnSpeed = 30.0f;
-        rb = GetComponent<Rigidbody>();
+        speed = 3000.0f;                      // Constant speed of vehicle
+        turnSpeed = 30.0f;                  // turn speed of vehicle
+        rb = GetComponent<Rigidbody>();     // short cut to rigidbody
    
     }
 
     // Updates vehicle postion per frame on Update
-    void Update()
+    void FixedUpdate()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);// position of vehicle ever frame update 
+        rb.AddRelativeForce(Vector3.forward * speed * verticalInput);
+        transform.Rotate(Vector3.up * turnSpeed * horizantalInput * Time.deltaTime);
 
     }
-
+    // Updates movements of vehicle on keypress WASD
     private void OnMove(InputValue input)
     {
-        verticalInput = input.Get<Vector2>()
+        verticalInput = input.Get<Vector2>().y;
+        horizantalInput = input.Get<Vector2>().x;
     }
 
 
